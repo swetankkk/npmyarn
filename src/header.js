76e6656689610807fa847f70999c99d2f85dc2e6
radicalswetank <swetank199@gmail.com>
2020-11-02 07:33:05 +0530

    Fixed CSS

--- src/header.js
+++ src/header.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import './header.css'
-const Header =()=>{return <div className="header"><div className="logo"><div>NPMYARN.DEV</div><div>NPM To Yarn Converter</div></div></div>};
+const Header =()=>{return <div className="header"><div className="logo">NPMYARN.DEV</div><div className="subheading">NPM To Yarn Command Converter Tool</div></div>};
 export default Header;
